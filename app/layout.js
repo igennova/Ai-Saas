679403533dfb995eab7cdfb81f95118281045a1b
igennova <luckynegi1025@gmail.com>
2025-02-26 13:20:54 +0530

    completed

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen"> {children}</main>
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200 ">
-                <p>Made by igennova</p>
+                <p>LUCKY NEGI</p>
               </div>
             </footer>
           </ThemeProvider>
